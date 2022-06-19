7d701c3c58c3eba3d659bc55f3f9803cee1949ba
leandrob0 <leandro.bovino@gmail.com>
2022-06-19 18:45:47 -0300

    header text added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,10 @@ import './Header.css';
 const Header = () => {
     return (
         <header className='header'>
-
+            <h1 className='header__title'>Where in the world?</h1>
+            <div className='header__mode-switch'>
+                <p>Dark mode</p>
+            </div>
         </header>
     )
 }
