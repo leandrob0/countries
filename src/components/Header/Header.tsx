58d3e3b0d0dbea0f504dc3b8c39f1fae9b962ff1
leandrob0 <leandro.bovino@gmail.com>
2022-06-19 18:52:54 -0300

    header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import './Header.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon as moonDark } from '@fortawesome/free-solid-svg-icons';
+import { faMoon as moonLight } from '@fortawesome/free-regular-svg-icons';
+
 const Header = () => {
     return (
         <header className='header'>
             <h1 className='header__title'>Where in the world?</h1>
             <div className='header__mode-switch'>
-                <p>Dark mode</p>
+                <FontAwesomeIcon icon={moonDark} />
+                <p className='mode-switch__text'>Dark mode</p>
             </div>
         </header>
     )
