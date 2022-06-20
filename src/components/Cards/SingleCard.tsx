f0f20ba67fa4e25416ea67582e5ded38ec4efbe4
leandrob0 <leandro.bovino@gmail.com>
2022-06-20 20:06:45 -0300

    capitals array formatted to be shown correctly

--- src/components/Cards/SingleCard.tsx
+++ src/components/Cards/SingleCard.tsx
@@ -1,30 +1,41 @@
 import { formattedAPIResponse } from "../../types";
 
 // create a new interface for prop types
-interface CardProps{
-    country: formattedAPIResponse;
- }
- 
-
-const SingleCard = ({country}: CardProps) => {
-    return (
-        <article className="card">
-            <img src={country.flags.png} alt={`${country.name.official} flag.`} className="card__image" />
-            <h2>{country.name.common}</h2>
-            <div className="card__text-container">
-                <p className="card__text-key">Population:&nbsp;</p>
-                <p>{country.population || 'Unknown'}</p>
-            </div>
-            <div className="card__text-container">
-                <p className="card__text-key">Region:&nbsp;</p>
-                <p>{country.region || 'Unknown'}</p>
-            </div>
-            <div className="card__text-container">
-                <p className="card__text-key">Capital:&nbsp;</p>
-                <p>{country.capital || 'Unknown'}</p>
-            </div>
-        </article>
-    )
+interface CardProps {
+  country: formattedAPIResponse;
 }
+
+const SingleCard = ({ country }: CardProps) => {
+  return (
+    <article className="card">
+      <img
+        src={country.flags.png}
+        alt={`${country.name.official} flag.`}
+        className="card__image"
+      />
+      <h2>{country.name.common}</h2>
+      <div className="card__text-container">
+        <p className="card__text-key">Population:&nbsp;</p>
+        <p>{country.population || "Unknown"}</p>
+      </div>
+      <div className="card__text-container">
+        <p className="card__text-key">Region:&nbsp;</p>
+        <p>{country.region || "Unknown"}</p>
+      </div>
+      <div className="card__text-container">
+        <p className="card__text-key">Capital:&nbsp;</p>
+        <p className="card__text-capital">
+          {country.capital !== undefined && country.capital.length !== 0
+            ? country.capital.map((capital, index) => {
+                return index + 1 === country.capital.length
+                  ? capital
+                  : capital + ", ";
+              })
+            : "Unknown"}
+        </p>
+      </div>
+    </article>
+  );
+};
 
 export default SingleCard;
