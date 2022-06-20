22c5640b9e96c58ebf03dca16c44d5abd12f813c
leandrob0 <leandro.bovino@gmail.com>
2022-06-19 21:40:52 -0300

    change event added to search, submit not yet.

--- src/components/FilterSection/Search.tsx
+++ src/components/FilterSection/Search.tsx
@@ -1,9 +1,22 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import React, { useState } from "react";
+
 const Search = () => {
+  const [country, setCountry] = useState<string>("");
+
+  const handleChange = (evt:React.ChangeEvent<HTMLInputElement>) => {
+    setCountry(evt.target.value);
+  }
+
+  const handleSubmit = (evt:React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    // Hace algo.
+  }
+
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={handleSubmit}>
       <button className="search-form__button">
         <FontAwesomeIcon
           icon={faMagnifyingGlass}
@@ -11,6 +24,9 @@ const Search = () => {
         />
       </button>
       <input
+        onChange={handleChange}
+        value={country}
+        name='country-search'
         placeholder="Search for a country..."
         className="search-form__input"
       />
