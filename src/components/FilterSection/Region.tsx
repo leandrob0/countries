253cbccafe90975a80caa63ea457fc3a3ae84c3b
leandrob0 <leandro.bovino@gmail.com>
2022-06-20 12:31:13 -0300

    modal showing regions added, animation for it added

--- src/components/FilterSection/Region.tsx
+++ src/components/FilterSection/Region.tsx
@@ -1,11 +1,21 @@
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+import RegionList from "./RegionList";
 
 const Region = () => {
+  const [showRegions, setShowRegions] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setShowRegions(!showRegions);
+  }
+
   return (
-    <div className="filter-region">
+    <div className="filter-region" onClick={handleClick}>
       <p className="filter-region__text">Filter by Region</p>
       <FontAwesomeIcon icon={faChevronDown} className="filter-region__icon"/>
+      {showRegions && <RegionList />}
     </div>
   );
 };
---
+++ src/components/FilterSection/RegionList.tsx
@@ -0,0 +1,14 @@
+const RegionList = () => {
+  return (
+    <div className="filter-options">
+      <p className="filter-options__text">All</p>
+      <p className="filter-options__text">Africa</p>
+      <p className="filter-options__text">America</p>
+      <p className="filter-options__text">Asia</p>
+      <p className="filter-options__text">Europe</p>
+      <p className="filter-options__text">Oceania</p>
+    </div>
+  );
+};
+
+export default RegionList;
