22f950502831347e0f7d069d141900507937fbfa
leandrob0 <leandro.bovino@gmail.com>
2022-06-20 19:32:50 -0300

    styles reseted

--- src/components/Cards/SingleCard.tsx
+++ src/components/Cards/SingleCard.tsx
@@ -10,6 +10,19 @@ const SingleCard = ({country}: CardProps) => {
     return (
         <article className="card">
             <img src={country.flags.png} alt={`${country.name.official} flag.`} className="card__image" />
+            <h2>{country.name.common}</h2>
+            <div className="card__text-container">
+                <p className="card__text-key">Population:</p>
+                <p>{country.population}</p>
+            </div>
+            <div className="card__text-container">
+                <p className="card__text-key">Region:</p>
+                <p>{country.region}</p>
+            </div>
+            <div className="card__text-container">
+                <p className="card__text-key">Capital:</p>
+                <p>{country.capital}</p>
+            </div>
         </article>
     )
 }
