eee7a3a191fd650dd1aab4d3d4b5bd4b1955e000
Leandro <75281214+leandrob0@users.noreply.github.com>
2022-06-22 11:33:41 -0300

    Filter by region functionality. (#4)

--- src/components/FilterSection/RegionList.tsx
+++ src/components/FilterSection/RegionList.tsx
@@ -1,12 +1,48 @@
+import {
+  getCountriesByRegion,
+  getAllCountries,
+} from "../../services/fetchCountriesData";
+
+import { useAppDispatch } from "../../hooks/redux";
+import { setCountries } from "../../features/countriesSlice";
+
 const RegionList = () => {
+  const dispatch = useAppDispatch();
+
+  const clickHandler = (evt: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
+    const target = evt.target as HTMLParagraphElement;
+    
+    if(target.id === 'all') {
+      getAllCountries()
+        .then(res =>  dispatch(setCountries(res)))
+        .catch(err => console.log(err));
+    } else {
+      getCountriesByRegion(target.id)
+        .then(res =>  dispatch(setCountries(res)))
+        .catch(err => console.log(err));
+    }
+  }
+
   return (
     <div className="filter-options">
-      <p id='all' className="filter-options__text">All</p>
-      <p id='africa' className="filter-options__text">Africa</p>
-      <p id='america' className="filter-options__text">America</p>
-      <p id='asia' className="filter-options__text">Asia</p>
-      <p id='europe' className="filter-options__text">Europe</p>
-      <p id='oceania' className="filter-options__text">Oceania</p>
+      <p onClick={clickHandler} id="all" className="filter-options__text">
+        All
+      </p>
+      <p onClick={clickHandler} id="africa" className="filter-options__text">
+        Africa
+      </p>
+      <p onClick={clickHandler} id="america" className="filter-options__text">
+        America
+      </p>
+      <p onClick={clickHandler} id="asia" className="filter-options__text">
+        Asia
+      </p>
+      <p onClick={clickHandler} id="europe" className="filter-options__text">
+        Europe
+      </p>
+      <p onClick={clickHandler} id="oceania" className="filter-options__text">
+        Oceania
+      </p>
     </div>
   );
 };
