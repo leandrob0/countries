bd2f32d910de86aac0bc5f064e3e64f1bdeca140
leandrob0 <leandro.bovino@gmail.com>
2022-06-22 11:41:58 -0300

    service to fetch data with a search done

--- src/services/fetchCountriesData.ts
+++ src/services/fetchCountriesData.ts
@@ -18,3 +18,11 @@ export const getCountriesByRegion = async (region: string): Promise<formattedAPI
 
     return formattedResponse;
 }
+
+export const getCountriesByQuery = async (query: string): Promise<formattedAPIResponse[]> => {
+    const data = await fetch(`${baseUrl}name/${query}`);
+    const jsonData = await data.json();
+    const formattedResponse = formatResponse(jsonData);
+
+    return formattedResponse;
+}
